8cfa0db9360854d3953c94b6eaa2bc5d095932c1
juniorvilas <log_junior@hotmail.com>
2022-05-17 12:19:11 -0300

    aula 01

---
+++ FRONTEND/AULA01/src/components/Input/index.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState, useContext, useRef } from "react";
+import { ContextoFormulario } from "../../context/contextoFormulario"
+
+
+// acrecenta uma props shouldFocus que informa se deve focar o input
+const Input = ({ name, label, type = "text",  shouldFocus = false,
+isPokemon = false, }) => {
+  // Aqui devemos acessar o estado global para obter os dados
+  // do formulário e uma maneira de atualizá-los.
+
+  const ref = useRef();
+
+  const { handleInputBlur, fomulario } = useContext(ContextoFormulario);
+
+
+  // Além disso, usaremos um estado local para lidar com o estado da input.
+  const [stateInput, setStateInput] = useState("");
+
+  const onChange = (e) => {
+    // Aqui devemos atualizar o estado local do input
+    setStateInput(e.target.value);
+  };
+
+  const onBlur = (e) => {
+    e.preventDefault();
+    // Aqui devemos atualizar o estado global com os dados de
+    // cada entrada.
+    // DICA: Podemos usar o nome de cada entrada para salvar
+    // os dados no estado global usando uma notação { chave: valor }
+    handleInputBlur(isPokemon ? "ATUALIZAR_POKEMON" : "ATUALIZAR_TREINADOR", {
+      campo: name,
+      valor: stateInput,
+    });
+  };
+
+  //Depois que a pagina carrega focamos no input usando a ref
+  useEffect(() => {
+    if (ref.current && shouldFocus) {
+      ref.current.focus();
+    }
+  }, [shouldFocus]);
+
+
+  return (
+    <div className="input-receptor">
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        value={stateInput}
+        id={name}
+        onChange={onChange}
+        onBlur={onBlur}
+        ref={ref}
+      />
+    </div>
+  );
+};
+
+export default Input;
